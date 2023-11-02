8ff45f2f4cb259d7c91f925773b7c9da93dfe611
hrimov <andrew.hrimov@gmail.com>
2023-11-02 17:48:47 +0100

    Add grafana-loki-vector logging stack

--- src/app/presentation/api/v1/controllers/healthcheck.py
+++ src/app/presentation/api/v1/controllers/healthcheck.py
@@ -1,5 +1,9 @@
+import logging
+
 from fastapi import APIRouter, status
 
+
+logger = logging.getLogger(__name__)
 
 healthcheck_router = APIRouter(
     prefix="/healthcheck",
@@ -9,4 +13,5 @@ healthcheck_router = APIRouter(
 
 @healthcheck_router.get("/", status_code=status.HTTP_200_OK)
 async def get_status() -> dict[str, str]:
+    logger.debug("Healthcheck endpoint called")
     return {"message": "ok"}
